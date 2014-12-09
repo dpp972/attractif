b5771078f5f7fc1019ef03702a2af56593d5a978
Xou <xrousset78800@gmail.com>
2014-12-09 15:31:24 +0100

    initial commit 2

---
+++ src/FrontOfficeBundle/Controller/DefaultController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace FrontOfficeBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class DefaultController extends Controller
+{
+    
+    /**
+     * @Route("/", name="homepage")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return array();
+    }
+    
+    /**
+     * @Route("/hello/{name}")
+     * @Template()
+     */
+    public function helloAction($name)
+    {
+        return array('name' => $name);
+    }
+}
